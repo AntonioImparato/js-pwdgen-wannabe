56dce424804df94aa054e2622e0c579c680b683f
AntonioImparato <antonioimparato16@gmail.com>
2024-05-06 21:53:46 +0200

    add: output consol

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 //input utente
 prompt("questo è l'inserimento dei tuoi dati ricorda che il primo numero deve essere maggiore rispetto al secondo ");
-const firstname = prompt('Inserisci il tuo nome');
-const lastname = prompt('Inserisci il tuo cognome');
+const firstName = prompt('Inserisci il tuo nome');
+const lastName = prompt('Inserisci il tuo cognome');
 const color = prompt('Inserisci il tuo colore preferito');
 const number1 = prompt('Inserisci il primo numero');
 const number2 = prompt('Inserisci il secondo numero');
 // input utente
 
+
+
 //calcolo numeri 
 const number3 = number1 / number2;
 //calcolo numeri
 
-const passw = firstname + lastname + color + number3;
-//output per l'utente
 
-document.getElementById('nome_utente').innerHTML = firstname;
-document.getElementById('cognome_utente').innerHTML = lastname;
+
+//generazione password
+const passw = firstName + lastName + color + number3;
+//generazione password
+
+
+//output per l'utente in consol
+
+const output = `
+-------------Password Generator-------------
+
+${firstName}${lastName}${color}${number3}
+
+--------------------------------------------
+`;
+console.log(output);
+//output per l'utente in consol
+
+
+
+
+//output per l'utente html
+document.getElementById('nome_utente').innerHTML = firstName;
+document.getElementById('cognome_utente').innerHTML = lastName;
 document.getElementById('colore').innerHTML = color;
 document.getElementById('numero1').innerHTML = number1;
 document.getElementById('numero2').innerHTML = number2;
 document.getElementById('pass').innerHTML = passw
+//output per l'utente html
